cec18dba72b86409b647eb5b0fd9ed9a4f09c412
L00177579 <L00177579@atu.ie>
2023-03-26 19:00:04 +0100

    initial .NET project

---
+++ ChatGPTPipelineTest.Test/GPTClassTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatGPTPipelineTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTPipelineTest.Tests
+{
+    [TestClass()]
+    public class GPTClassTests
+    {
+        [TestMethod()]
+        [DataRow("test")]
+        public void gptfunctionTest(string test)
+        {
+            GPTClass gPTClass = new GPTClass();
+            Assert.IsTrue(gPTClass.gptfunction(test));
+        }
+    }
+}
---
+++ ChatGPTPipelineTest/GPTClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTPipelineTest
+{
+    public class GPTClass
+    {
+        public GPTClass()
+        {
+
+        }
+
+        public bool gptfunction(string test)
+        {
+            if (test == "test")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
